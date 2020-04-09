dba99afa09c4b409697f9f2746dabb17e9ace166
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2020-04-08 23:39:54 -0700

    initial commit

---
+++ app/src/main/java/com/example/identitas_test_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.identitas_test_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
